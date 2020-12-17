a04c6ae85abc08ff68f9eec26650951fe8f114fd
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-17 10:45:20 +0000

    added react-router-dom import, and also installed it for *this* project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import About from "./components/About";
+import Home from "./components/Home";
+import Pricing from "./components/Pricing";
 
 const App = ()=> {
   
