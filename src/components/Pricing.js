712c9585b4077361d9a86ce38cc0dc7f00912d4f
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-17 12:25:03 +0000

    commented out console.log from pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Pricing = ({prices}) => {
 
-  console.log({prices});
+  // console.log({prices});
 
   const priceList = prices.map((price) => {
   return <li>{price.level}: £{price.cost}</li>
