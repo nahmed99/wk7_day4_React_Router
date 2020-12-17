10df5c4dd372f639600ac7b2b00c898fd28eb8ab
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-17 12:15:58 +0000

    Pricing receives the data and presents it on the screen

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -5,10 +5,16 @@ const Pricing = ({prices}) => {
 
   console.log({prices});
 
+  const priceList = prices.map((price) => {
+  return <li>{price.level}: £{price.cost}</li>
+  });
+
   return (
     <>
       <h4>Pricing</h4>
-      <p>££££££</p>
+      <ul>
+        {priceList}
+      </ul>
     </>
   );
 }
